3d0eef2de74c228da76e2b36d5c1672354ffc642
soulcounter486-lgtm <soulcounter486@gmail.com>
2026-04-12 23:41:29 +0700

    \StaffPos: add useRouter import - fix build error"

--- components/StaffPos.tsx
+++ components/StaffPos.tsx
@@ -1,6 +1,7 @@
 'use client';
 
 import { useEffect, useMemo, useState } from 'react';
+import { useRouter } from 'next/navigation';
 import { getSupabase } from '@/lib/supabaseClient';
 
 type Product = {
